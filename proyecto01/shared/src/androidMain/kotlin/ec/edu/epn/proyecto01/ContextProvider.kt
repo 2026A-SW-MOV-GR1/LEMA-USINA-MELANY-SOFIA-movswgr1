a0d26fff581c778076e6f7e5576174f6c4253460
melanysofial <melany.lema01@epn.edu.ec>
2026-06-05 02:19:26 -0500

    proyecto01

---
+++ proyecto01/shared/src/androidMain/kotlin/ec/edu/epn/proyecto01/ContextProvider.kt
@@ -0,0 +1,5 @@
+package ec.edu.epn.proyecto01
+
+object ContextProvider {
+    lateinit var applicationContext: Context
+}
---
+++ proyecto01/shared/src/androidMain/kotlin/ec/edu/epn/proyecto01/StoragePlatform.kt
@@ -0,0 +1,4 @@
+package ec.edu.epn.proyecto01
+
+class StoragePlatform {
+}
